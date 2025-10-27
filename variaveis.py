36a91f0dc9615fe26423a31c45db1d08e3aa16a8
Rodrigo Fricks <RODRIGOFR@dpc.ad>
2025-10-27 15:59:51 -0300

    Regras

--- variaveis.py
+++ variaveis.py
@@ -1,3 +1,10 @@
 nome = "Maria"
 idade = 25 
 altura = 1.68
+
+#REGRAS
+#Devem começar com uma letra ou _
+#Podem conter letar, numeros e _
+#Não podem começar com números
+#Sensíveis a maiusculas/minusculas ( idade <> de Idade)
+#Evite usar palavras reservadas ( como , if, for, class, etc.)
